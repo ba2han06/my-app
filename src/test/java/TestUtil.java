faab4e35b7c6ec713c3dddfe521e43aeb075915c
ba2han06 <batuhangunal2003@gmail.com>
2024-03-31 03:18:27 +0300

    Son ekleme.

--- src/test/java/TestUtil.java
+++ src/test/java/TestUtil.java
@@ -13,4 +13,54 @@ public class TestUtil {
 
   @Test
   public void example() { assertTrue(true); }
+
+  @Test
+    public void compute_OneArgument_ReturnsFalse() {
+        Util util = new Util();
+        
+        boolean result = util.compute(5);
+        
+         assertFalse(result);
+    }
+
+    @Test
+    public void compute_EvenNumberOfArguments_ReturnsFalse() {
+        Util util = new Util();
+        
+        boolean result = util.compute(1, 2, 3, 4);
+        
+         assertFalse(result);
+    }
+
+   @Test
+    public void compute_ArgumentEqualToZero_ThrowsRuntimeException() {
+        Util util = new Util();
+        
+        boolean exceptionThrown = false;
+        try {
+            util.compute(0, 2, 0);
+        } catch (RuntimeException e) {
+            exceptionThrown = true;
+        }
+        
+        assertTrue(exceptionThrown);
+    }
+
+    @Test
+    public void compute_SumDivisibleByArgument_ReturnsTrue() {
+        Util util = new Util();
+        
+        boolean result = util.compute(1, 2, 3);
+        
+        assertTrue(result);
+    }
+
+    @Test
+    public void compute_NoSumDivisibleByArgument_ReturnsFalse() {
+        Util util = new Util();
+        
+        boolean result = util.compute(3, 6, 5);
+        
+        assertFalse(result);
+    }
 }
